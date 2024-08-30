83014fbf0082b407c30371b9792cce5d9a978949
mateus <22.01082-3@maua.br>
2024-08-30 08:28:31 -0300

    previsao_do_tempo

--- previsao_do_tempo/index.js
+++ previsao_do_tempo/index.js
@@ -1,14 +1,45 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const axios = require('axios')
+const dotenv = require('dotenv');
+const input = require('input');
+dotenv.config();
+const axios = require('axios');
 
-const PROTOCOL = process.env.PROTOCOL
-const BASE_URL = process.env.BASE_URL
-const Q = process.env.Q
-const APP_ID = process.env.APP_ID
+(async () => {
+    const { PROTOCOL, BASE_URL, APP_ID, CNT, UNITS, IDIOM } = process.env;
 
-console.log(BASE_URL)
+    const cidade = await input.text('Cidade: ');
 
-const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APP_ID}`
+    const url = `${PROTOCOL}://${BASE_URL}?q=${cidade}&appid=${APP_ID}&cnt=${CNT}&units=${UNITS}&lang=${IDIOM}`;
 
-console.log(url)
+    await axios.get(url)
+        .then(async res => {
+            return res.data;
+        })
+        .then(res => {
+            console.log(`CNT: ${res.cnt}`);
+            return res;
+        })
+        .then(async res => {
+            for (let previsao of res['list']) {
+                let data = new Date(previsao.dt * 1000);
+                let tempMin = previsao.main.temp_min;
+                let tempMax = previsao.main.temp_max;
+                let feelsLike = previsao.main.feels_like;
+                let description = previsao.weather[0].description;
+                console.log(`
+                    data: ${data.toLocaleString()}
+                    temp min: ${tempMin}\u00B0
+                    temp max: ${tempMax}\u00B0
+                    description: ${description}
+                    feels like: ${feelsLike}
+                `);
+            }
+            return res.list;
+        })
+        .then(res => {
+            const totalFilter = res.filter((previsao) => +previsao.main.feels_like > 20).length;
+            console.log(totalFilter);
+        })
+        .catch(error => {
+            console.error('Erro:', error.data);
+        });
+})();
